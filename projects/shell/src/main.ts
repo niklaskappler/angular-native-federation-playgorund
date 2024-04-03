544abe754fc00b49a33a7555f706311e683cbf9f
Niklas Kappler <mail@niklas-kappler.de>
2024-04-03 18:22:01 +0200

    make nf application of apps

--- projects/shell/src/main.ts
+++ projects/shell/src/main.ts
@@ -1,6 +1,10 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { initFederation } from '@angular-architects/native-federation';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+initFederation({
+  'mfe1': 'http://localhost:4201/remoteEntry.json',
+  'mfe2WithRouting': 'http://localhost:4202/remoteEntry.json',
+  'mfe3': 'http://localhost:4203/remoteEntry.json'
+})
+  .catch(err => console.error(err))
+  .then(_ => import('./bootstrap'))
+  .catch(err => console.error(err));
